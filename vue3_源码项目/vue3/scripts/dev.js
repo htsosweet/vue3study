97e585f93c0387f44f51185beb2fb50919784f27
heet <hxhsyx@qq.com>
2025-07-01 23:23:22 +0800

    esbuild打包配置

--- vue3_源码项目/vue3/scripts/dev.js
+++ vue3_源码项目/vue3/scripts/dev.js
@@ -2,6 +2,7 @@ import { parseArgs } from 'node:util';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import esbuild from 'esbuild';
+import { createRequire } from 'node:module';
 
 const { values: { format }, positionals } = parseArgs({
     allowPositionals: true,
@@ -20,18 +21,20 @@ const __dirname = dirname(__filename)
 
 const target = positionals.length ? positionals[0] : 'vue'
 const entry = resolve(__dirname, `../packages/${target}/src/index.ts`)
+const require = createRequire(import.meta.url)
+
+const pkg = require(`../packages/${target}/package.json`)
 esbuild.context({
     entryPoints: [entry],//入口文件
     outfile: resolve(__dirname, `../packages/${target}/dist/${target}.${format}.js`),//输出的文件
     format,
     bundle: true,
     platform: format === 'cjs' ? 'node' : 'browser',//打包平台
-    sourcemap: true,//开启sourcemap方便调试
+    sourcemap: true,//开启sourcemap方便调试,
+    globalName: pkg.buildOptions?.name,
 }).then(ctx => ctx.watch())
 
 
-console.log(__filename);
-console.log(__dirname);
-console.log(target);
-console.log(entry);
+console.log(pkg);
+
 
